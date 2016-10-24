8e23bcd60a98f4d227728f3295d11a3b1e1a6e3c
takeshi mizugaki <takeshi.mizugaki@ast-net.co.jp>
2016-10-24 17:08:53 +0900

    ADD : WKWebViewでサイトの表示、ATSの無効化設定

--- mockWKWebView/ViewController.swift
+++ mockWKWebView/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+
+    var webView: WKWebView!
+
+    override func loadView() {
+        let webConfiguration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: webConfiguration)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        webView.allowsBackForwardNavigationGestures = true
+        view = webView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // WebViewのATSを無効化
+        // Info.plist -> App Transport Security Settings -> Allow Arbitrary Loads in Web Content = YES
+        let myURL = URL(string: "http://www.yahoo.co.jp/")
+        let myRequest = URLRequest(url: myURL!)
+        webView.load(myRequest)
     }
 
     override func didReceiveMemoryWarning() {
